0b8cbde6b609e178cf6f915fc8d8576044087450
yahiaanane <57045927+yahiaanane@users.noreply.github.com>
2023-02-19 13:28:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const submitButton = document.getElementById('submit-button');
+const promptInput = document.getElementById('prompt-input');
+const outputContainer = document.querySelector('.output-container');
+
+submitButton.addEventListener('click', async () => {
+  const prompt = promptInput.value;
+  const response = await getResponse(prompt);
+  outputContainer.innerHTML += `<p><strong>You:</strong> ${prompt}</p>`;
+  outputContainer.innerHTML += `<p><strong>OpenAI:</strong> ${response}</p>`;
+  promptInput.value = '';
+  outputContainer.scrollTop = outputContainer.scrollHeight;
+});
+
+async function getResponse(prompt) {
+  const response = await fetch('/chatgpt', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ prompt })
+  });
+  const { choices } = await response.json();
+  return choices[0].text;
+}
