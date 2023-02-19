2e76f9c801e1e158f1e52667b4607f029e44865f
yahiaanane <57045927+yahiaanane@users.noreply.github.com>
2023-02-19 21:32:05 +0100

    Update app.py

--- back-end/app.py
+++ back-end/app.py
@@ -1,24 +1,38 @@
 import openai
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 openai.api_key = "YOUR_API_KEY_HERE"
 
-@app.route('/chatgpt', methods=['POST'])
-def chat_gpt():
-    data = request.get_json()
-    prompt = data['prompt']
-    completions = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    message = completions.choices[0].text
-    return jsonify(choices=completions.choices)
+class ChatGptHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        data = json.loads(body)
+
+        prompt = data['prompt']
+        completions = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        message = completions.choices[0].text
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        response = {"choices": completions.choices}
+        self.wfile.write(json.dumps(response).encode())
+
+def run(server_class=HTTPServer, handler_class=ChatGptHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Listening at: http://localhost:{port}')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-    app.run()
+    run()
